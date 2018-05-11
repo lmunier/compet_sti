8e01dcf8f413482870ca5e39f565b9f9cb7479ff
Munier Louis <louis.munier@epfl.ch>
2018-05-12 01:24:44 +0200

    Add tests communications

---
+++ test_haar/test_haar_cascade.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*-coding:Utf-8 -*
+#lm020518.1510
+
+# File to test haar-cascade filter on faces
+
+import numpy as np
+import cv2 as cv
+
+image = cv.imread('/home/pi/EPFL-Media-Library/image32_largeprvw.jpeg',0)
+bottle_cascade = cv.CascadeClassifier('/home/pi/compet_sti/test_haar/data/cascade.xml')
+bottle = bottle_cascade.detectMultiScale(image, 1.3, 5)
+
+for (x, y, w, h) in bottle:
+    cv.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    roi_color = image[y:y+h, x:x+w]
+
+cv.imshow('Image', image)
+cv.waitKey(0)
