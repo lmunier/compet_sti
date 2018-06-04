f030beb84e1ee3be92980822b8ae8e7a34840b98
Munier Louis <louis.munier@epfl.ch>
2018-06-04 23:22:39 +0200

    Update

--- bottles_tracking/b_tracking.cpp
+++ bottles_tracking/b_tracking.cpp
@@ -13,20 +13,49 @@ void init_pins(){
     pinMode(LED_PIN, OUTPUT);
 }
 
+// Raspicam initialization
+RaspiCam_Cv init_raspicam(){
+    RaspiCam_Cv camera;
+
+    camera.set(CAP_PROP_FRAME_HEIGHT, HEIGHT_IMAGE);
+    camera.set(CAP_PROP_FRAME_WIDTH, WIDTH_IMAGE);
+
+    return camera;
+}
+
 // OpenCV function to detect bottles
 int bottles_scanning(){
+    // Initialize variables
+    Mat image;
+
+    // Initialization of variables
+    // Set distance variables
+    int bottle_x_pos = 0;
+    int bottle_y_pos = 0;
+
+    // Set blur kernel
+    int kernel_blur = 3;
+
+    // Extract max, min
+    Point minLoc, maxLoc;
+    double min=0.0, max=0.0;
+
     wiringPiSetup();
     init_pins();
 
-    int c = 5;
+    // Initialize camera
+    RaspiCam_Cv camera = init_raspicam();
 
-    while(c > 0){
-        led_enable(true);
-        delay(5000);
-        //led_enable(false);
-        //delay(5000);
+    // Open camera
+    if(!camera.open()) {
+        cout << "ERROR: can not open camera" << endl;
+        return -1;
+    }
 
-        //c--;
+    while(true){
+        camera.grab();
+        camera.retrieve(image);
+        imshow("Image", image);
     }
 
     return 1;
