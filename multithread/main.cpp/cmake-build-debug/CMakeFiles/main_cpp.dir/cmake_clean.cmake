85e4f41c2a1d43808ee51a988a46c5a439224f4f
Munier Louis <louis.munier@epfl.ch>
2018-06-06 15:53:35 +0200

    Multithread
    Hello_world multithread

---
+++ multithread/main.cpp/cmake-build-debug/CMakeFiles/main_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_cpp.dir/main.cpp.obj"
+  "main_cpp.pdb"
+  "main_cpp.exe"
+  "main_cpp.exe.manifest"
+  "libmain_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
