c23369915b4a96ae7f21388113a5f6764c2fdc1d
Louis Munier <louis.munier@epfl.ch>
2018-06-06 14:31:23 +0200

    Update

--- hello_world_read.py
+++ hello_world_read.py
@@ -8,14 +8,15 @@ import serial
 
 port = serial.Serial("/dev/ttyAMA0",
 	baudrate=9600,
-#        parity=serial.PARITY_NONE,
-#        stopbits=serial.STOPBITS_ONE,
-#        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
         timeout=1)
 
 while True:
 	rcv = port.readline()
-	str = repr(rcv)
+#	str = rcv.decode("utf-8")
+#	str = repr(rcv)
 
 #	if str != "b''":
-	print(str)
+	print(rcv)
